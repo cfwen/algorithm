58c2343ecd85d727502cfaac207cc18371795923
unknown <whywe@live.com>
2020-03-21 16:59:48 -0400

    setup

---
+++ include/binary-search.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "data-structure.h"
+
+int binary_search(vector<int>& arr, int target)
+{
+    int left = 0;
+    int right = arr.size()-1;
+    while(left <= right)
+    {
+        int mid = left + (right-left)/2;
+        if(arr[mid] < target)
+            left = mid+1;
+        else if(arr[mid] > target)
+            right = mid-1;
+        else
+            return mid;
+    }
+    return -1;
+}
+
+TEST(binary_search_test, target_exist) {
+    vector<int> arr1{ 1,2,3,5,8,9 };
+    EXPECT_EQ(0, binary_search(arr1, 1));
+    EXPECT_EQ(5, binary_search(arr1, 9));
+    EXPECT_EQ(3, binary_search(arr1, 5));
+    EXPECT_EQ(-1, binary_search(arr1, 10));
+}
---
+++ include/data-structure.h
@@ -0,0 +1,58 @@
+
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+struct TreeLinkNode
+{
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x) : val(x), left(nullptr), right(nullptr), next(nullptr) {}
+};
+
+struct ListNode
+{
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+struct RandomListNode
+{
+    int label;
+    RandomListNode *next, *random;
+    RandomListNode(int x) : label(x), next(nullptr), random(nullptr) {}
+};
+
+class TrieNode
+{
+  public:
+    bool end;
+    TrieNode* next[26];
+    // Initialize
+    TrieNode()
+    {
+        fill(next, next + 26, nullptr);
+        end = false;
+    }
+};
